38c552f9333966a6e274729f9d5b7cab93735fce
Mike Verbanic <verbanicm@google.com>
2023-02-10 14:15:53 -0500

    make terraform changes for testing

--- test/projects/app-a/main.tf
+++ test/projects/app-a/main.tf
@@ -1,19 +1,11 @@
 locals {
   project_id               = "verbanicm-dev"
-  name                     = "test-app-a"
+  name                     = "app-a"
   ci_service_account_email = "autotf-ci-sa@verbanicm-dev.iam.gserviceaccount.com"
 }
 
-resource "google_service_account" "run_service_account" {
+resource "google_service_account" "test_service_account" {
   project      = local.project_id
   account_id   = "${local.name}-sa"
-  display_name = "${local.name}-sa Cloud Run Service Account"
-}
-
-module "cloud_run" {
-  source                = "git::https://github.com/abcxyz/terraform-modules.git//modules/cloud_run?ref=246cd2f48ca0e2f9c34492ceb16833f2279f64e7"
-  project_id            = local.project_id
-  name                  = local.name
-  image                 = "gcr.io/cloudrun/hello:latest"
-  service_account_email = google_service_account.run_service_account.email
+  display_name = "${local.name}-sa Service Account"
 }
