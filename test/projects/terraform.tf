99c0117d97ad657a7d784e047c655e48d2279163
Mike Verbanic <verbanicm@users.noreply.github.com>
2023-02-27 12:29:17 -0500

    add license headers (#26)

--- test/projects/terraform.tf
+++ test/projects/terraform.tf
@@ -12,13 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-locals {
-  project_id = "verbanicm-dev"
-  name       = "app-a"
-}
+terraform {
+  required_version = ">= 1.0.0"
 
-resource "google_service_account" "test_service_account" {
-  project      = local.project_id
-  account_id   = "${local.name}-sa"
-  display_name = "${local.name}-sa Service Account"
+  required_providers {
+    google = {
+      version = ">= 4.45"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "verbanicm-dev-terraform"
+    prefix = "state/projects"
+  }
 }
