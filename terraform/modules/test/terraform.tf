0265620e2b8b52298ff0c8fe05c4086ecd1c367f
Mike Verbanic <verbanicm@users.noreply.github.com>
2023-03-09 09:31:17 -0500

    composite actions (#42)
    ## READY
    
    Modified process to make use of Composite GitHub Actions. This process
    leaves the existing reusable workflows as a dependency for existing
    "customers", only shifts to make use of the new Composite GitHub Action.
    
    This allows us to decouple authentication tokens from the implementation
    of Guardian.
    
    This is a temporary solution to allow use to start using Guardian
    internally. A more full-featured and performant action should be written
    in the future.

--- terraform/modules/test/terraform.tf
+++ terraform/modules/test/terraform.tf
@@ -12,6 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-module "app-a" {
-  source = "./app-a"
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = ">= 5.0"
+    }
+    google = {
+      version = ">= 4.45"
+    }
+  }
 }
