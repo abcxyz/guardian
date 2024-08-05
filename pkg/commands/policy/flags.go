ddcac74cf6c2f95f85b8003f383db75b92a649b9
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2024-08-05 20:10:19 +0000

    Add policy command

---
+++ pkg/commands/policy/flags.go
@@ -0,0 +1,44 @@
+// Copyright 2024 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package policy
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/abcxyz/pkg/cli"
+)
+
+type PolicyFlags struct {
+	ResultsFile string
+}
+
+func (e *PolicyFlags) Register(set *cli.FlagSet) {
+	f := set.NewSection("POLICY OPTIONS")
+
+	f.StringVar(&cli.StringVar{
+		Name:    "results-file",
+		Example: "results.json",
+		Target:  &e.ResultsFile,
+		Usage:   "The path to a JSON file containing the OPA eval result.",
+	})
+
+	set.AfterParse(func(existingError error) (merr error) {
+		if e.ResultsFile == "" {
+			merr = errors.Join(merr, fmt.Errorf("missing flag: results-file is required"))
+		}
+		return merr
+	})
+}
