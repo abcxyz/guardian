21ac2dd191b0c10ef3d67d0b5d749b70500e7bdf
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2024-08-06 18:01:27 +0000

    Fix lint

--- pkg/commands/policy/policy_test.go
+++ pkg/commands/policy/policy_test.go
@@ -18,9 +18,10 @@ import (
 	"context"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+
 	"github.com/abcxyz/pkg/logging"
 	"github.com/abcxyz/pkg/testutil"
-	"github.com/google/go-cmp/cmp"
 )
 
 const (
