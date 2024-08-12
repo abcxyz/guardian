cff14f9eeae08bbd99d54f88eb90cca6e31a640e
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2024-08-12 18:31:34 +0000

    Don't append

--- pkg/github/github_mock.go
+++ pkg/github/github_mock.go
@@ -255,7 +255,7 @@ func (m *MockGitHubClient) RequestReviewers(ctx context.Context, owner, repo str
 	}
 
 	return &RequestReviewersResponse{
-		Users: append(m.UserReviewers, users...),
-		Teams: append(m.TeamReviewers, teams...),
+		Users: m.UserReviewers,
+		Teams: m.TeamReviewers,
 	}, nil
 }
