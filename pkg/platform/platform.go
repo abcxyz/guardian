6e1acc6df814b9569b809dc6d40bd4a9b2fe0d33
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2024-08-16 22:57:14 +0000

    Fix lint

--- pkg/platform/platform.go
+++ pkg/platform/platform.go
@@ -24,9 +24,10 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/posener/complete/v2"
+
 	"github.com/abcxyz/guardian/pkg/github"
 	"github.com/abcxyz/pkg/cli"
-	"github.com/posener/complete/v2"
 )
 
 const (
