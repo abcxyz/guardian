7fe44361e3c5e27232f8f13e4a70321d633bbe89
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2024-08-20 16:59:51 +0000

    Remove extra package comment

--- pkg/platform/github.go
+++ pkg/platform/github.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package github provides the functionality to send requests to the GitHub API.
 package platform
 
 import (
