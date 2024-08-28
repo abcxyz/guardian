d606ebe2829462e53c12f2cc0303eaa7ab7d1241
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2024-08-28 20:59:37 +0000

    Fix function name in doc comment

--- pkg/platform/local.go
+++ pkg/platform/local.go
@@ -31,7 +31,7 @@ func (l *Local) AssignReviewers(ctx context.Context, inputs *AssignReviewersInpu
 	return &AssignReviewersResult{}, nil
 }
 
-// GetLatestApprovers returns an empty result.
+// GetPolicyData returns an empty result.
 func (l *Local) GetPolicyData(ctx context.Context) (*GetPolicyDataResult, error) {
 	return &GetPolicyDataResult{}, nil
 }
