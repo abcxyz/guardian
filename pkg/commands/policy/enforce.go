a7ccc810df3a17ba3c9a266545a06976f9a8180d
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2024-11-11 22:11:28 +0000

    Add EnforceDenyAll

--- pkg/commands/policy/enforce.go
+++ pkg/commands/policy/enforce.go
@@ -34,6 +34,7 @@ import (
 // Result defines the expected structure of the OPA policy evaluation result.
 type Result struct {
 	MissingApprovals []*MissingApproval `json:"missing_approvals"`
+	DenyAll          []*DenyAll         `json:"deny_all"`
 }
 
 // MissingApproval defines the missing approvals determined from the policy
@@ -143,6 +144,10 @@ func (c *EnforceCommand) Process(ctx context.Context) error {
 		if err := c.EnforceMissingApprovals(ctx, b, k, v); err != nil {
 			merr = errors.Join(merr, err)
 		}
+
+		if err := c.EnforceDenyAll(ctx, b, k, v); err != nil {
+			merr = errors.Join(merr, err)
+		}
 	}
 
 	if err := c.reporter.Status(ctx, reporter.StatusPolicyViolation, &reporter.StatusParams{
@@ -202,4 +207,25 @@ func (c *EnforceCommand) EnforceMissingApprovals(ctx context.Context, b strings.
 	}
 
 	return merr
+}
+
+// DenyAll defines the expected structure of deny all violations from the policy
+// evaluation result.
+type DenyAll struct {
+	Message string `json:"msg"`
+}
+
+// EnforceDenyAll blocks the action if a deny all violation is found and reports
+// any violations with the detailed error message.
+func (c *EnforceCommand) EnforceDenyAll(ctx context.Context, b strings.Builder, policyName string, r *Result) error {
+	logger := logging.FromContext(ctx)
+
+	fmt.Fprintf(&b, "#### %s\n", policyName)
+	for _, m := range r.DenyAll {
+		return fmt.Errorf("failed: \"%s\" - %s", policyName, m.Message)
+	}
+
+	logger.DebugContext(ctx, "no deny all violation for policy",
+		"policy_name", policyName)
+	return nil
 }
