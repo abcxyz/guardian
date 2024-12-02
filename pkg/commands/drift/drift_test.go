eadd1a7bdc8ae915baa2a5cbd644556226435889
Dylan Yurgionas <dcreey@google.com>
2024-12-02 23:17:01 +0000

    chore: add test for drift message

--- pkg/commands/drift/drift_test.go
+++ pkg/commands/drift/drift_test.go
@@ -210,3 +210,92 @@ func TestDrift_DetectDrift(t *testing.T) {
 		})
 	}
 }
+
+func TestDrift_driftMessage(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name  string
+		drift *IAMDrift
+		want  string
+	}{
+		{
+			name: "success_no_drift",
+			drift: &IAMDrift{
+				ClickOpsChanges:         map[string]*assetinventory.AssetIAM{},
+				MissingTerraformChanges: map[string]*TerraformStateIAMSource{},
+			},
+		},
+		{
+			name: "success_all_click_ops_drift",
+			drift: &IAMDrift{
+				ClickOpsChanges: map[string]*assetinventory.AssetIAM{
+					"/organizations/1231231/folders/123123123123/roles/viewer/group:my-group@google.com":                                                  folderViewer,
+					"/organizations/1231231/projects/my-project/roles/compute.admin/serviceAccount:my-service-account@my-project.iam.gserviceaccount.com": projectAdmin,
+					"/organizations/1231231/roles/browser/group:my-group@google.com":                                                                      orgGroupBrowser,
+					"/organizations/1231231/roles/browser/serviceAccount:my-service-account@my-project.iam.gserviceaccount.com":                           orgSABrowser,
+					"/organizations/1231231/roles/browser/user:dcreey@google.com":                                                                         orgUserBrowser,
+				},
+				MissingTerraformChanges: map[string]*TerraformStateIAMSource{},
+			},
+			want: `Found Click Ops Changes
+| ID | Resource ID | Member | Role |
+|----|-------------|--------|------|
+|/organizations/1231231/folders/123123123123/roles/viewer/group:my-group@google.com|123123123123|group:my-group@google.com|roles/viewer|
+|/organizations/1231231/projects/my-project/roles/compute.admin/serviceAccount:my-service-account@my-project.iam.gserviceaccount.com|1231232222|serviceAccount:my-service-account@my-project.iam.gserviceaccount.com|roles/compute.admin|
+|/organizations/1231231/roles/browser/group:my-group@google.com|1231231|group:my-group@google.com|roles/browser|
+|/organizations/1231231/roles/browser/serviceAccount:my-service-account@my-project.iam.gserviceaccount.com|1231231|serviceAccount:my-service-account@my-project.iam.gserviceaccount.com|roles/browser|
+|/organizations/1231231/roles/browser/user:dcreey@google.com|1231231|user:dcreey@google.com|roles/browser|
+`,
+		},
+		{
+			name: "success_all_missing_terraform_drift",
+			drift: &IAMDrift{
+				ClickOpsChanges: map[string]*assetinventory.AssetIAM{},
+				MissingTerraformChanges: map[string]*TerraformStateIAMSource{
+					"/organizations/1231231/folders/123123123123/roles/viewer/group:my-group@google.com": {
+						AssetIAM:     folderViewer,
+						StateFileURI: statefileURI,
+					},
+					"/organizations/1231231/projects/my-project/roles/compute.admin/serviceAccount:my-service-account@my-project.iam.gserviceaccount.com": {
+						AssetIAM:     projectAdmin,
+						StateFileURI: statefileURI,
+					},
+					"/organizations/1231231/roles/browser/group:my-group@google.com": {
+						AssetIAM:     orgGroupBrowser,
+						StateFileURI: statefileURI,
+					},
+					"/organizations/1231231/roles/browser/serviceAccount:my-service-account@my-project.iam.gserviceaccount.com": {
+						AssetIAM:     orgSABrowser,
+						StateFileURI: statefileURI,
+					},
+					"/organizations/1231231/roles/browser/user:dcreey@google.com": {
+						AssetIAM:     orgUserBrowser,
+						StateFileURI: statefileURI,
+					},
+				},
+			},
+			want: `Found Missing Terraform Changes
+| ID | StateFile URI | Resource ID | Member | Role |
+|----|---------------|-------------|--------|------|
+|/organizations/1231231/folders/123123123123/roles/viewer/group:my-group@google.com|gs://my-bucket/default.tf|123123123123|group:my-group@google.com|roles/viewer|
+|/organizations/1231231/projects/my-project/roles/compute.admin/serviceAccount:my-service-account@my-project.iam.gserviceaccount.com|gs://my-bucket/default.tf|1231232222|serviceAccount:my-service-account@my-project.iam.gserviceaccount.com|roles/compute.admin|
+|/organizations/1231231/roles/browser/group:my-group@google.com|gs://my-bucket/default.tf|1231231|group:my-group@google.com|roles/browser|
+|/organizations/1231231/roles/browser/serviceAccount:my-service-account@my-project.iam.gserviceaccount.com|gs://my-bucket/default.tf|1231231|serviceAccount:my-service-account@my-project.iam.gserviceaccount.com|roles/browser|
+|/organizations/1231231/roles/browser/user:dcreey@google.com|gs://my-bucket/default.tf|1231231|user:dcreey@google.com|roles/browser|
+`,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			got := driftMessage(tc.drift)
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("driftMessage() returned diff (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
