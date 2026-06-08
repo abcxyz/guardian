8cc3dacaa656a5468d01b730aa6cc6ab1817891d
Rivoc Xu <rivocpxu@google.com>
2026-06-08 20:42:33 +0000

    lint

--- pkg/commands/workflows/report_test.go
+++ pkg/commands/workflows/report_test.go
@@ -18,10 +18,10 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/abcxyz/pkg/testutil"
 	"github.com/google/go-cmp/cmp"
 
 	"github.com/abcxyz/guardian/pkg/platform"
+	"github.com/abcxyz/pkg/testutil"
 )
 
 func TestReportCommandFlags(t *testing.T) {
